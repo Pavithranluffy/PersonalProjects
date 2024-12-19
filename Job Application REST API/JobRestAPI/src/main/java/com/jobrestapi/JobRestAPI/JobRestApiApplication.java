823b268ef466308ef0ffffa28549654d221c802d
Pavithran <pavithran@calsoftgroup.com>
2024-12-19 22:56:49 +0530

    JobRestAPI

---
+++ Job Application REST API/JobRestAPI/src/main/java/com/jobrestapi/JobRestAPI/JobRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.jobrestapi.JobRestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobRestApiApplication.class, args);
+	}
+
+}
---
+++ Job Application REST API/JobRestAPI/src/test/java/com/jobrestapi/JobRestAPI/JobRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobrestapi.JobRestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
